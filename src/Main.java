ab7c3c38bae215d2360c7c6f0a1b6b7709ab0c21
Кирилл Петров <kirillv1993@yandex.ru>
2022-05-31 13:25:04 +0400

    расчет количества начисленных миль

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int tiket = 500;
-        int one_mile = 20;
+        int oneMile = 20;
 
-        int bonus = tiket / one_mile;
+        int bonus = tiket / oneMile;
         System.out.println(bonus);
     }
 }
