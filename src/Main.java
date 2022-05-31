184f83c6e0d53c184baf3e4d11d05d78cdeda371
Кирилл Петров <kirillv1993@yandex.ru>
2022-05-31 11:44:38 +0400

    java dz2.1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int tiket = 500;
+        int one_mile = 20;
+
+        int bonus = tiket / one_mile;
+        System.out.println(bonus);
+    }
+}
